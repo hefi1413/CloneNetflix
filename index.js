9a5c24f97387f43e7ebe9108a70f49bf2ceab50b
alexmeirelles <alemeirelles@msn.com>
2022-03-28 19:47:01 -0300

    require dotenv

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded());
+
+
 
 
 const catalogo = [
