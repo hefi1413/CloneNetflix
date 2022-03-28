2fd8485d63a125426e56553a1130eccd1b04cb2b
alexmeirelles <alemeirelles@msn.com>
2022-03-28 15:55:42 -0300

    criando objeto

--- index.js
+++ index.js
@@ -7,42 +7,34 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded());
 
+
 const catalogo = [
   {
       id: 1,
-      numero: 319,
-      nome: "Sharpedo",
-      tipo: "Water",
-      imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/319.png",
+      nome: "Jurassic Park",
+      ano: 1993,
+      genero: "Aventura",
+      tipo: "filme",
       descricao:"As soon as it catches the scent of prey, Sharpedo will jet seawater from its backside, hurtling toward the target to attack at 75 mph.",
-      altura: 1.8,
-      peso: 88.8,
-      categoria: "Brutal",
-      habilidade: "Rough Skin",
+      imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/319.png",
   },
   {
-      id: 2,
-      numero: 247,
-      nome: "Pupitar",
-      tipo: "Rock",
-      imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/247.png",
-      descricao:"Even sealed in its shell, it can move freely. Hard and fast, it has outstanding destructive power.",
-      altura: 1.2,
-      peso: 152,
-      categoria: "Hard Shell",
-      habilidade: "Shed Skin",
+    id: 2,
+    nome: "O Demolidor",
+    ano: 1994,
+    genero: "Policial",
+    tipo: "filme",
+    descricao:"As soon as it catches the scent of prey, Sharpedo will jet seawater from its backside, hurtling toward the target to attack at 75 mph.",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/319.png",
   },
   {
-      id: 3,
-      numero: 302,
-      nome: "Sableye",
-      tipo: "Dark",
-      imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/302.png",
-      descricao:"This Pokémon is feared. When its gemstone eyes begin to glow with a sinister shine, it’s believed that Sableye will steal people’s spirits away.",
-      altura: 0.5,
-      peso: 11,
-      categoria: "Darkness",
-      habilidade: "Stall",
+    id: 3,
+    nome: "Jurassic Park",
+    ano: 1994,
+    genero: "Aventura",
+    tipo: "filme",
+    descricao:"As soon as it catches the scent of prey, Sharpedo will jet seawater from its backside, hurtling toward the target to attack at 75 mph.",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/319.png",
   }
 ]
 
